06e118bc6b6dbfd1cc6a371779dbdcc646bfc761
wzb <leimiliya@hotmail.com>
2021-08-09 00:51:50 +0800

    1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { TsReactTest } from "screens/project-list/try-use-array";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list/index";
 
 function App() {
-  return <div className="App">ProjectListScreen</div>;
+  return (
+    <div className="App">
+      <TsReactTest></TsReactTest>
+      {/* <ProjectListScreen></ProjectListScreen> */}
+    </div>
+  );
 }
 
 export default App;
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -39,3 +39,18 @@ export const useDebounce = <V>(value: V, delay?: number) => {
 //     }, delay);
 //   }
 // }
+
+export const useArray = <T>(initialArray: T[]) => {
+  const [value, setValue] = useState(initialArray);
+  return {
+    value,
+    setValue,
+    add: (item: T) => setValue([...value, item]),
+    clear: () => setValue([]),
+    removeIndex: (index: number) => {
+      const copy = [...value];
+      copy.splice(index, 1);
+      setValue(copy);
+    },
+  };
+};
