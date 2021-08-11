a9fc5d1fe98bc1054419909048fb7eb5cf692f82
wzb <leimiliya@hotmail.com>
2021-08-11 22:14:07 +0800

    jsonServe模拟登录

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import { TsReactTest } from "screens/project-list/try-use-array";
 import "./App.css";
 import { ProjectListScreen } from "./screens/project-list/index";
+import { LoginScreen } from "./screens/login/index";
 
 function App() {
   return (
     <div className="App">
-      <TsReactTest></TsReactTest>
+      <LoginScreen></LoginScreen>
+      {/* <TsReactTest></TsReactTest> */}
       {/* <ProjectListScreen></ProjectListScreen> */}
     </div>
   );
